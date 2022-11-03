a6bcdbcb9e02257fd919a7afabb9bade36012719
manda <mandanovita7@gmail.com>
2022-11-03 09:14:23 +0700

    add env

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ import smtplib, os
 from dotenv import load_dotenv
 
 
-load_dotenv(".env")
+load_dotenv("Users/manda/.env")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
